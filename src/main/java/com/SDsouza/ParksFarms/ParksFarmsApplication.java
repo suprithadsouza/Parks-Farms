6c39e32d9bea04a4000b0835191fa5c9d1df576d
suprithadsouza <135457210+suprithadsouza@users.noreply.github.com>
2024-08-26 13:35:25 -0400

    Parks

---
+++ src/main/java/com/SDsouza/ParksFarms/ParksFarmsApplication.java
@@ -0,0 +1,13 @@
+package com.SDsouza.ParksFarms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParksFarmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParksFarmsApplication.class, args);
+	}
+
+}
